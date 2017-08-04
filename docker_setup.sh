4611a5fd930c887757b6aaf65dd66d2b579140f1
Marc Robert Wong <marcrobertwong@gmail.com>
2017-08-04 10:38:25 -0700

    Updated setup script comment for . ./docker_setup.

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,4 +1,6 @@
 # Script for setting up the share-button-study Docker image from scratch.
+# Run using . ./docker_setup.sh as a shortcut for `source docker_setup.sh && ./docker_setup.sh`.
+
 #!/usr/bin/env bash
 cd /share-button-study
 yarn install
